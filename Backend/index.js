580b622a1fa6aec202ff1763526f5ce0a6431936
Manish <iammanish2109@gmail.com>
2023-03-29 21:10:07 +0530

    google oauth

--- Backend/index.js
+++ Backend/index.js
@@ -5,12 +5,32 @@ const {authenticator} = require("./middleware/authentication.js");
 const cookieParser = require('cookie-parser');
 const cors = require("cors");
 app = express();
+const {passport} = require("./google-auth")
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
 require("dotenv").config();
+
+app.get("/", (req, res) => {
+   res.send("Home Page")
+})
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile','email'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login',session:false }),
+   function (req, res) {
+     console.log(req.user);
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+
+
+
 
 app.use("/users",usersRoute);
 // app.use(authenticator)
