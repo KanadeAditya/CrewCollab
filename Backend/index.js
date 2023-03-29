7f3458a4a63ffe95f008a5cff4eaab3e6bb277a0
Abhinav068 <abhinav28897@gmail.com>
2023-03-29 22:58:23 +0530

    chatform done

--- Backend/index.js
+++ Backend/index.js
@@ -1,65 +1,65 @@
 const express = require("express");
 const { connection, client } = require("./db.js");
-const  {usersRoute} = require("./controller/user.routes.js");
-const {authenticator} = require("./middleware/authentication.js");
-const {formatMsg}=require('./utils/message');
+const { usersRoute } = require("./controller/user.routes.js");
+const { authenticator } = require("./middleware/authentication.js");
+const { formatMsg } = require('./utils/message');
 require("dotenv").config();
 const cors = require("cors");
 
 
-const {passport} = require("./google-auth")
+const { passport } = require("./google-auth")
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-const http=require('http').createServer(app);
-const {Server}=require('socket.io');
-const io= new Server(http);
+const http = require('http').createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(http);
 
 //////////////////////
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
    console.log('connected a new user');
 
-   socket.on("joinRoom",({username,room})=>{
+   socket.on("joinRoom", ({ username, room }) => {
 
-    //    const user=userJoin({id:socket.id,username,room});
-       socket.join(user.room);
+      //    const user=userJoin({id:socket.id,username,room});
+      socket.join(user.room);
 
-       socket.emit("message",formatMsg('ChatMe',"Welcome to ChatMe server."));
+      socket.emit("message", formatMsg('ChatMe', "Welcome to ChatMe server."));
 
-       socket.broadcast.to(user.room).emit("message",formatMsg('ChatMe',`${username} has joined the chat`));
+      socket.broadcast.to(user.room).emit("message", formatMsg('ChatMe', `${username} has joined the chat`));
 
-       io.to(user.room).emit("roomUsers",{
-           room:user.room,
-           users:getRoomUsers(user.room) 
-       }) ;
+      io.to(user.room).emit("roomUsers", {
+         room: user.room,
+         users: getRoomUsers(user.room)
+      });
 
    })
 
-   socket.on("chatMsg",(msg)=>{
-      
-    //    const user = getCurrentUser(socket.id);
+   socket.on("chatMsg", (msg) => {
 
-       io.to(user.room).emit("message",formatMsg(user.username,msg));
+      //    const user = getCurrentUser(socket.id);
 
-    });  
+      io.to(user.room).emit("message", formatMsg(user.username, msg));
 
-   socket.on('disconnect',()=>{ 
+   });
 
-    //    const user=userLeave(socket.id);
+   socket.on('disconnect', () => {
 
-       socket.broadcast.to(user.room).emit("message",formatMsg('ChatMe',`${user.username} has left the chat`));
+      //    const user=userLeave(socket.id);
 
-       io.to(user.room).emit("roomUsers",{
-           room:user.room,
-        //    users:getRoomUsers(user.room)
-       }) 
+      socket.broadcast.to(user.room).emit("message", formatMsg('ChatMe', `${user.username} has left the chat`));
+
+      io.to(user.room).emit("roomUsers", {
+         room: user.room,
+         //    users:getRoomUsers(user.room)
+      })
    })
 })
 //////////////////////
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
    // Login page will be sent from here
    res.status(200).send('Welcome to SlackBot....');
 })
@@ -69,29 +69,29 @@ app.get("/", (req, res) => {
 })
 
 app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile','email'] }));
+   passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login',session:false }),
+app.get('/auth/google/callback',
+   passport.authenticate('google', { failureRedirect: '/login', session: false }),
    function (req, res) {
-     console.log(req.user);
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
+      console.log(req.user);
+      // Successful authentication, redirect home.
+      res.redirect('/');
+   });
 
 
 
 
 
-app.use("/users",usersRoute);
+app.use("/users", usersRoute);
 // app.use(authenticator)
 
-http.listen(process.env.port,async ()=>{
+http.listen(process.env.port, async () => {
    try {
-    await connection;
-    console.log("Connected to MongoDB");
+      await connection;
+      console.log("Connected to MongoDB");
    } catch (error) {
-    console.log({"error":error.message});
+      console.log({ "error": error.message });
    }
    console.log(`Running at port: ${process.env.port}`);
 })
