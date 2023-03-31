e0cbfb37c6afb84d26a540225d5c2efc9f25d1ba
KanadeAditya <110182805+KanadeAditya@users.noreply.github.com>
2023-03-31 23:43:49 +0530

    messages pipelines added

---
+++ Backend/controller/message.controller.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const mongoose = require("mongoose")
+const usersRoute = express.Router();
+const {UserModel} =require("../model/user.model.js");
+const {MessageModel} =require("../model/message.model.js");
+const {RoomModel} =require("../model/room.model.js");
+require("dotenv").config();
+const jwt = require("jsonwebtoken")
+const {authenticator} = require("../middleware/authentication.js");
+
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr(process.env.encryptionkey);
+
+
+
+const messegerouter = express.Router();
+
+messegerouter.use(authenticator)
+
+messegerouter.get("/",(req,res)=>{
+    res.send("THESE ARE MESSAGES")
+})
+
+messegerouter.get("/roomsConnected",async(req,res)=>{
+    let email = req.body.email;
+    let rooms = await UserModel.aggregate([
+        {
+          '$lookup': {
+            'from': 'message', 
+            'localField': 'email', 
+            'foreignField': 'email', 
+            'as': 'message'
+          }
+        }, {
+          '$unwind': {
+            'path': '$message'
+          }
+        }, {
+          '$group': {
+            '_id': '$email', 
+            'rooms': {
+              '$push': '$message.roomname'
+            }
+          }
+        }, {
+          '$match': {
+            '_id': `${email}`
+          }
+        }
+      ]);
+    //   console.log(email)
+
+      res.send(rooms)
+})
+
+messegerouter.get("/roomsMessage/:roomname",async (req,res)=>{
+    let {roomname} = req.params;
+    let users = await RoomModel.aggregate([
+        {
+          '$lookup': {
+            'from': 'message', 
+            'localField': 'roomname', 
+            'foreignField': 'roomname', 
+            'as': 'message'
+          }
+        }, {
+          '$match': {
+            'roomname': `${roomname}`
+          }
+        }
+      ])
+
+      res.send(users)
+})
+
+
+module.exports = {messegerouter};
