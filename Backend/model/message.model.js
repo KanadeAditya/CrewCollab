0ce6f61e915a36fc51196e84921a1640a38b01f1
KanadeAditya <110182805+KanadeAditya@users.noreply.github.com>
2023-04-01 23:39:36 +0530

    updated pipeline

--- Backend/model/message.model.js
+++ Backend/model/message.model.js
@@ -4,7 +4,8 @@ const messageschema = mongoose.Schema({
     email:{type:String,required:true},
     message:{type:String,required:true},
     userID:{type:String,required:true},
-    roomname : {type:String,required:true},
+    roomname:{type:String,required:true},
+    roomID:{type:String,required:true},
     time: {type:Date , required : true}
 })
 
